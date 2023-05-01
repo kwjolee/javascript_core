e09791a3efc606c73df0e429dd84ec0e5e6a34d4
Kwang Lee <kwjolee@gmail.com>
2023-05-01 15:15:52 -0600

    clean up mortgage calculator

--- backend/JS101/Lesson2/mortgage_calculator/mortgage_calculator.js
+++ backend/JS101/Lesson2/mortgage_calculator/mortgage_calculator.js
@@ -1,106 +1,114 @@
-///// requirements
 const readline = require('readline-sync');
+const MESSAGES = require ('./messages.json');
 
-// convert message to system prompt
 function prompt(message) {
   console.log(`\n===> ${message}\n`);
 }
 
-// get loan amount (principal)
-function getPrincipal() {
-  prompt("What is the loan amount? ($)");
-  let principal = readline.question("$ ");
-  while (invalidNumber(principal)) {
-    prompt("Invalid entry. Please enter a number:");
-    prompt("What is the loan amount? ($)");
-    principal = readline.question("$ ");
+function askUnit(item) {
+  if (item === "askPrincipal") return "$";
+  let allowedUnits = MESSAGES["allowedUnits"][item];
+  prompt(MESSAGES["units"][item]);
+  let unit = readline.question();
+  while (!allowedUnits.includes(unit)) {
+    prompt(MESSAGES["invalidGeneral"]);
+    unit = readline.question();
   }
-  return Number(principal);
+  return convertUnits(unit);
 }
 
-// get APR (must decide % or decimal)
-// monthly is just APR / 12
-// return in number
-function getMonthlyAPR() {
-  prompt("What is the Annual Percentage Rate? (%)");
-  let APR = readline.question();
-  while (invalidNumber(APR)) {
-    prompt("Invalid entry. Please enter a number:");
-    prompt("What is the Annual Percentage Rate? (%)");
-    APR = readline.question();
+function convertUnits(unit) {
+  let unitConverted;
+  if (unit[0] === "p") {
+    unitConverted = "%";
+  } else if (unit[0] === "d") {
+    unitConverted = "";
+  } else if (unit[0] === "y") {
+    unitConverted = "years";
+  } else if (unit[0] === "m") {
+    unitConverted = "months";
   }
-  return Number(APR) / 12 / 100;
+  return unitConverted;
 }
 
-// get loan duration in years
-// return in number
-function getLoanDuration() {
-  prompt("What is the loan duration? (years)");
-  let loanDuration = readline.question();
-  while (invalidNumber(loanDuration)) {
-    prompt("Invalid entry. Please enter a number:");
-    prompt("What is the loan duration?");
-    loanDuration = readline.question();
+function getResponse(item) {
+  let unit = askUnit(item);
+  prompt(`${MESSAGES[item]} (${unit})`);
+  let response = readline.question();
+  while (invalidNumber(response, item)) {
+    prompt(MESSAGES["invalidResponse"]);
+    prompt(`${MESSAGES[item]} (${unit})`);
+    response = readline.question();
   }
-  return Number(loanDuration * 12);
+  return convertResponse(Number(response), item, unit);
 }
 
-// calculate monthly interest rate
+function invalidNumber(numberString, item) {
+  if (isNaN(Number(numberString)) || (numberString === '')) {
+    return true;
+  } else if (item === "askPrincipal" && Number(numberString) < 0) {
+    return true;
+  } else if (item === "askAPR" && Number(numberString) < 0) {
+    return true;
+  } else if (item === "askLoanDuration" && Number(numberString) <= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function convertResponse(response, item, unit) {
+  if (item === "askAPR") {
+    if (unit === "%") {
+      response /= 1200;
+    } else {
+      response /= 12;
+    }
+  } else if (unit === "years") {
+    response *= 12;
+  }
+  return response;
+}
+
 function calcMonthlyPay(principal, APR, loanDuration) {
   if (APR === 0) return principal / loanDuration;
   return principal * (APR / (1 - Math.pow((1 + APR), (-loanDuration))));
 }
 
-// check if input is a valid number
-function invalidNumber(numberString) {
-  return isNaN(Number(numberString)) || numberString === '';
-}
 
-// print payment amount as dollar and cents $123.45
 function showPaymentAmount(MonthlyPay) {
-  prompt(`Your monthly payment amount is $${MonthlyPay.toFixed(2)}.`);
+  prompt(`${MESSAGES["paymentAffix"]} $${MonthlyPay.toFixed(2)}.`);
 }
 
-// ask if another calculation is wanted
 function askDoAgain() {
   let allowedEntries = ['y', 'n', 'yes', 'no'];
-  prompt("Would you like to do another payment calculation? (y)es/(n)o");
+  prompt(MESSAGES["askDoAgain"]);
   let doAgain = readline.question().toLowerCase();
   while (!allowedEntries.includes(doAgain)) {
-    prompt("Invalid entry. Please enter (y)es or (n)o");
+    prompt(MESSAGES["invalidRedo"]);
     doAgain = readline.question().toLowerCase();
   }
+  console.clear();
   return doAgain[0] === "y";
 }
 
 function exitCalc() {
-  console.clear();
-  prompt("Thank you for using Mortage Calculator.");
+  prompt(MESSAGES["exit"]);
 }
 
-
-///// UI/UX
-// personal touch?
-
-
-///// edge cases
-// zero interest loan
-// non integer number of years
-
-
-///// run program
-
+//
 console.clear();
 prompt("Welcome to the Mortgage Calculator");
 
 do {
-
-  let principal = getPrincipal();
-  let APR = getMonthlyAPR();
-  let loanDuration = getLoanDuration();
-  let MonthlyPay = calcMonthlyPay(principal, APR, loanDuration);
-
-  showPaymentAmount(MonthlyPay);
-
+  let principal = getResponse("askPrincipal");
+  if (principal === 0) {
+    prompt(MESSAGES["noLoan"]);
+  } else {
+    let APR = getResponse("askAPR");
+    let loanDuration = getResponse("askLoanDuration");
+    let MonthlyPay = calcMonthlyPay(principal, APR, loanDuration);
+    showPaymentAmount(MonthlyPay);
+  }
 } while (askDoAgain());
 exitCalc();
